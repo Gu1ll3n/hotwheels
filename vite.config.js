ab93fb365b7dacf99e00e46ed505a5d8355ec470
Gu1ll3n <139734879+Gu1ll3n@users.noreply.github.com>
2026-06-10 00:22:27 -0400

    Limpiar errores de conflicto

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -6,12 +5,3 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
-=======
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
->>>>>>> bff2d54d576840c5c008b924a1f723d4eb146bb7
